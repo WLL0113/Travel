3214bd4f27e1faa9353a0ca9b40f6ede8539ed9b
LanLan <37686713+WLL0113@users.noreply.github.com>
2018-08-25 15:52:10 +0800

    'city-header'

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+    <div class="title">城市选择</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+ @import '~styles/varibles.styl'
+  .header
+    position: relative
+    overflow:hidden
+    height: $headerHeight
+    line-height: $headerHeight
+    font-size: .30rem
+    color: #fff
+    background: $bgColor
+
+    .header-back
+        position: absolute
+        width: .80rem
+        float:left
+        text-align:center
+        font-weight: bold
+        color: #fff
+    .title
+      text-align: center
+</style>
