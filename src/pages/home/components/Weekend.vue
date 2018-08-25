852f9fb8cecd7706000b0fe902b2426e7818ce6d
LanLan <37686713+WLL0113@users.noreply.github.com>
2018-08-25 14:52:50 +0800

    'index_finish'

--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="title">周末去哪儿</div>
     <ul>
-      <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
+      <li class="item border-bottom" v-for="item of list" :key="item.id">
         <div class="item-img-wrapper">
           <img class="item-img" :src="item.imgUrl">
         </div>
@@ -18,25 +18,8 @@
 <script>
 export default {
   name: 'HomeWeekend',
-  data () {
-    return {
-      weekendList: [{
-        id: '001',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/c2/2f36b1a2ff4976.jpg_r_640x214_d7471872.jpg',
-        title: '丽江必游TOP10',
-        desc: '爱巍峨雄伟的雪山，波澜壮阔的河川'
-      }, {
-        id: '002',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4f/a09503e4b9ee85.jpg_r_640x214_3c128df5.jpg',
-        title: '昆明必游TOP10',
-        desc: '走进春城，感受彩云之南'
-      }, {
-        id: '003',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/e2/2a63e38b84bc8e.jpg_r_640x214_679f0d37.jpg',
-        title: '玩转主题乐园',
-        desc: '乐园，与青春作伴'
-      }]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
@@ -53,7 +36,7 @@ export default {
   .item-img-wrapper
     overflow: hidden
     height: 0
-    padding-bottom: 33.9%
+    padding-bottom: 37.09%
   .item-img
     width: 100%
   .item-info
