4821be7ff9b86dfddabf85bc520955bba1ec95d7
LanLan <37686713+WLL0113@users.noreply.github.com>
2018-08-25 16:36:36 +0800

    'search'

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+ @import '~styles/varibles.styl'
+  .search
+    background: $bgColor
+    padding: 0 .1rem
+    height: .73rem
+    font-size: .24rem
+    .search-input
+      box-sizing: border-box
+      width:100%
+      text-align: center
+      padding: 0 .1rem
+      height: .62rem
+      line-height: .62rem
+      border-radius: .09rem
+      color: #666
+</style>
